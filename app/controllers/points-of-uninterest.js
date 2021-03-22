23ac9bfe9b7e4d89dd5f27e0e34deaac59ba324d
Esioan <esioan667@gmail.com>
2021-03-22 23:52:33 +0000

    poui list refactored to show only the poui's of the current user

--- app/controllers/points-of-uninterest.js
+++ app/controllers/points-of-uninterest.js
@@ -5,6 +5,8 @@ const user = require("../models/user");
 const { populate } = require("../models/user");
 const User = require("../models/user");
 const Joi = require('@hapi/joi');
+const { notImplemented } = require("@hapi/boom");
+const { isValidObjectId } = require("mongoose");
 
 const PointsOfUninterest = {
 
@@ -16,111 +18,122 @@ const PointsOfUninterest = {
 
   report: {
     handler: async function (request, h) {
-      const pointsOfUninterest = await PointOfUninterest.find().populate("creator").lean();
-      return h.view("report", {
-        title: "POI's to Date",
-        pointsOfUninterest: pointsOfUninterest,
-      });
-    },
-  },
-
-  createPOUI: {
-    handler: async function (request, h) {
-      const id = request.auth.credentials.id;
-      const user = await User.findById(id);
-      const data = request.payload;
-      const newPointOfUninterest = new PointOfUninterest({
-        name: data.name,
-        category: data.category,
-        description: data.description,
-        creator: user._id
-      });
-      await newPointOfUninterest.save();
-      return h.redirect("/home");
-    },
-  },
-
-  viewPOUI: {
-    handler: async function (request, h) {
       try {
-        const poui = await PointOfUninterest.findById(request.params._id);
-        const user = await User.findById(poui.creator);
-        console.log("Viewing POUI" + poui);
-        return h.view("view-poui", {
-          title: "View POUI",
-          name: poui.name,
-          category: poui.category,
-          description: poui.description,
-          creator: user.firstName + " " + user.lastName,
+        const id = request.auth.credentials.id;
+        const user = await User.findById(id);
+        console.log("User ID: " + id);
+        console.log(user.firstName);
+        const pointsOfUninterest = await PointOfUninterest.find({ creator: id }).populate("creator").lean();
+        // console.log(pointsOfUninterest)
+        return h.view("report", {
+          title: "POI's to Date",
+          pointsOfUninterest: pointsOfUninterest,
+          name: user.firstName,
         });
-      } catch (err) {
-        return h.view("view-poui"), { errors: [{ message: err.message }] }
-      }
-    }
-  },
-
-  editPOUIPage: {
-    handler: async function (request, h) {
-      try {
-        const poui = await PointOfUninterest.findById(request.params._id);
-        return h.view("edit-poui", {
-          title: "Edit POUI",
-          name: poui.name,
-          category: poui.category,
-          description: poui.description });
-      } catch (err) {
-        return h.view("login", { errors: [{ message: err.message }] });
-      }
-    },
-  },
-
-  editPOUI: {
-    validate: {
-      payload: {
-        name: Joi.string().required(),
-        category: Joi.string().required(),
-        description: Joi.string().required(),
-      },
-      options: {
-        abortEarly: false,
-      },
-      failAction: function (request, h, error) {
-        return h
-          .view("edit-poui", {
-            title: "Update POUI error",
-            errors: error.details,
-          })
-          .takeover()
-          .code(400);
-      },
-    },
-    handler: async function (request, h) {
-      try {
-        const pouiEdit = request.payload;
-        const poui = await PointOfUninterest.findById(request.params._id);
-        poui.name = pouiEdit.name;
-        poui.category = pouiEdit.category;
-        poui.description = pouiEdit.description;
-        await poui.save();
-        return h.redirect('/view-poui', pouiEdit);
-      } catch (err) {
-        return h.view("report", { errors: [{ message: error.message }] });
-      }
-    }
-  },
-
-  deletePOUI: {
-    handler: async function (request, h) {
-      try {
-        const poui = await PointOfUninterest.findById(request.params._id);
-        console.log("Removing POUI: " + poui);
-        await poui.remove();
-        return h.redirect("/report");
       } catch (err) {
         return h.view("report"), { errors: [{ message: err.message }] }
       }
     }
   },
-};
 
-module.exports = PointsOfUninterest;
+    createPOUI: {
+      handler: async function (request, h) {
+        const id = request.auth.credentials.id;
+        const user = await User.findById(id);
+        const data = request.payload;
+        const newPointOfUninterest = new PointOfUninterest({
+          name: data.name,
+          category: data.category,
+          description: data.description,
+          creator: user._id
+        });
+        await newPointOfUninterest.save();
+        return h.redirect("/home");
+      },
+    },
+
+    viewPOUI: {
+      handler: async function (request, h) {
+        try {
+          const poui = await PointOfUninterest.findById(request.params._id);
+          const user = await User.findById(poui.creator);
+          console.log("Viewing POUI" + poui);
+          return h.view("view-poui", {
+            title: "View POUI",
+            name: poui.name,
+            category: poui.category,
+            description: poui.description,
+            creator: user.firstName + " " + user.lastName,
+          });
+        } catch (err) {
+          return h.view("view-poui"), { errors: [{ message: err.message }] }
+        }
+      }
+    },
+
+    editPOUIPage: {
+      handler: async function (request, h) {
+        try {
+          const poui = await PointOfUninterest.findById(request.params._id);
+          return h.view("edit-poui", {
+            title: "Edit POUI",
+            name: poui.name,
+            category: poui.category,
+            description: poui.description
+          });
+        } catch (err) {
+          return h.view("login", { errors: [{ message: err.message }] });
+        }
+      },
+    },
+
+    editPOUI: {
+      validate: {
+        payload: {
+          name: Joi.string().required(),
+          category: Joi.string().required(),
+          description: Joi.string().required(),
+        },
+        options: {
+          abortEarly: false,
+        },
+        failAction: function (request, h, error) {
+          return h
+            .view("edit-poui", {
+              title: "Update POUI error",
+              errors: error.details,
+            })
+            .takeover()
+            .code(400);
+        },
+      },
+      handler: async function (request, h) {
+        try {
+          const pouiEdit = request.payload;
+          const poui = await PointOfUninterest.findById(request.params._id);
+          poui.name = pouiEdit.name;
+          poui.category = pouiEdit.category;
+          poui.description = pouiEdit.description;
+          await poui.save();
+          return h.redirect('/view-poui', pouiEdit);
+        } catch (err) {
+          return h.view("report", { errors: [{ message: error.message }] });
+        }
+      }
+    },
+
+    deletePOUI: {
+      handler: async function (request, h) {
+        try {
+          const poui = await PointOfUninterest.findById(request.params._id);
+          console.log("Removing POUI: " + poui);
+          await poui.remove();
+          return h.redirect("/report");
+        } catch (err) {
+          return h.view("report"), { errors: [{ message: err.message }] }
+        }
+      }
+    },
+  };
+
+  module.exports = PointsOfUninterest;
