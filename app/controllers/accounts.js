9b83911f36fd0274043316301f942df936f33a03
Esioan <esioan667@gmail.com>
2021-05-19 00:20:08 +0100

    hashing and salting of passwords implemented

--- app/controllers/accounts.js
+++ app/controllers/accounts.js
@@ -3,6 +3,9 @@
 const User = require('../models/user');
 const Boom = require('@hapi/boom');
 const Joi = require('@hapi/joi');
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 
 const Accounts = {
     index: {
@@ -48,11 +51,14 @@ const Accounts = {
                     const message = "Email address is already registered";
                     throw Boom.badData(message);
                 }
+
+                const hash = await bcrypt.hash(payload.password, saltRounds);
+
                 const newUser = new User({
                     firstName: payload.firstName,
                     lastName: payload.lastName,
                     email: payload.email,
-                    password: payload.password
+                    password: hash
                 });
                 user = await newUser.save();
                 request.cookieAuth.set({ id: user.id });
@@ -98,7 +104,7 @@ const Accounts = {
                     const message = "Email address is not registered";
                     throw Boom.unauthorized(message);
                 }
-                user.comparePassword(password);
+                await user.comparePassword(password);
                 request.cookieAuth.set({ id: user.id });
                 return h.redirect("/report");
             } catch (err) {
@@ -136,7 +142,8 @@ const Accounts = {
                 user.firstName = userEdit.firstName;
                 user.lastName = userEdit.lastName;
                 user.email = userEdit.email;
-                user.password = userEdit.password;
+                const hash = await bcrypt.hash(userEdit.password, saltRounds);
+                user.password = hash;
                 await user.save();
                 return h.redirect('/settings');
             } catch (err) {
